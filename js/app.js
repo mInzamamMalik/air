cd6b54254cd2596bbec1ad65e7d89391238c9fc1
Muhammad <azhar40@live.co.uk>
2021-01-08 19:32:07 +0500

    ip config

--- js/app.js
+++ js/app.js
@@ -177,8 +177,6 @@ function todo() {
         
             var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
             var text1 = text.replace(exp, '<a  target="_blank" href="$1">$1</a>');
-        
-    
             var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
             var replaced = text1.replace(exp2, '$1<a target="_blank" href="http://$2">$2</a>');
         
