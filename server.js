76a35f792c15340db5f44c8c25d46c1c5eca1649
Muhammad <azhar40@live.co.uk>
2021-01-08 19:23:38 +0500

    ip config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var cors = require("cors");
 var server = express();
+
 var PORT = process.env.PORT || 5000;
 
 server.use(cors());
@@ -12,6 +13,6 @@ server.get("/getIp",(req,res)=>{
         console.log("ip is==> "+ip);
     })
 
-server.listen(PORT,()=>{
+server.listen(PORT, ()=>{
     console.log("Server is running on port: "+PORT);
 })
